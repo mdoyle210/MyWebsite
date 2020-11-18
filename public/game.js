a7881f9ac05392cc3e531e33240b9dc167fcb2d8
mdoyle210 <70218157+mdoyle210@users.noreply.github.com>
2020-11-18 10:11:28 -0800

    added links to github and tech spec

---
+++ public/game.js
@@ -0,0 +1,16 @@
+//alert("Game Content Loaded");
+
+function removeGame(){
+  $("#game_content").html("");
+}
+$('#remove_game').click(removeGame);
+
+function funsies(){
+  alert("uh oh");
+}
+$('#remove_game').click(funsies);
+
+
+//$('#start_game').click(function() {
+//  $('#game_content').html('game.html');
+//})
