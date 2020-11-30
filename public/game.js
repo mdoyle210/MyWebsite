e94c3bb3acdab8dddc70b3b4d8eb096fd05df181
mdoyle210 <70218157+mdoyle210@users.noreply.github.com>
2020-11-30 09:18:05 -0800

    blah blah

--- public/game.js
+++ public/game.js
@@ -1,3 +1,13 @@
+$("#play_a_game").click(ajaxTheGame);
+
+function ajaxTheGame(){
+  $.ajax({url: "https://www.bubblegame.org/flash-bubble-spinner.php", success: loadContent});
+}
+
+function loadContent(result) {
+  $("#game_content").html(result);
+}
+
 alert("Game Content Loaded");
 
 $('#remove_game').click(function() {
